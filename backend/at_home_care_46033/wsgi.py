a5ebeda4df5e0ae44eef605edcea4917f8c0b9d5
Crowdbotics <team@crowdbotics.com>
2024-02-06 15:07:17 +0000

    Initial commit

---
+++ backend/at_home_care_46033/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for at_home_care_46033 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "at_home_care_46033.settings"
+)
+
+application = get_wsgi_application()
